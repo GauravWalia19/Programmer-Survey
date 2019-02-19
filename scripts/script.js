cad9a264983d60d94c04731ee22187ec507945fe
GauravWalia19 <gauravwalia019@gmail.com>
2019-02-20 00:33:05 +0530

    added js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,21 @@
 function hello()
 {
-    alert("success");
+    showsmile();
+}
+
+function showsmile()
+{
+    let box = document.getElementById('after-submit');
+    box.style.display="block";   
+    document.getElementsByTagName('section')[0].style.opacity="0.2";
+    document.getElementById('title').style.opacity="0.2";
+    document.getElementsByTagName('body')[0].style.backgroundColor="gray";
+}
+function hidesmile()
+{
+    let box = document.getElementById('after-submit');
+    box.style.display="none";
+    document.getElementsByTagName('section')[0].style.opacity="1";
+    document.getElementById('title').style.opacity="1";
+    document.getElementsByTagName('body')[0].style.backgroundColor="cyan";
 }
