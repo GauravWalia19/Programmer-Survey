94b29bb87c7d3f52eac9dd0468feba7aa01979fc
GauravWalia19 <gauravwalia019@gmail.com>
2019-02-23 23:07:33 +0530

    fixed the bugs

--- scripts/script.js
+++ scripts/script.js
@@ -5,17 +5,25 @@ function hello()
 
 function showsmile()
 {
-    let box = document.getElementById('after-submit');
+    let box = document.getElementById('modal');
     box.style.display="block";   
-    document.getElementsByTagName('section')[0].style.opacity="0.2";
-    document.getElementById('title').style.opacity="0.2";
-    document.getElementsByTagName('body')[0].style.backgroundColor="gray";
+    document.getElementsByTagName('section')[0].style.opacity="0";
+    document.getElementById('title').style.opacity="0";
+    document.getElementsByTagName('body')[0].style.backgroundColor="cyan";
 }
 function hidesmile()
 {
-    let box = document.getElementById('after-submit');
+    let a = window.innerHeight;
+    let b = window.innerWidth;
+
+    let box = document.getElementById('modal');
     box.style.display="none";
     document.getElementsByTagName('section')[0].style.opacity="1";
     document.getElementById('title').style.opacity="1";
-    document.getElementsByTagName('body')[0].style.backgroundColor="cyan";
+    if(a<=689 && b<=425)
+    {
+        document.getElementsByTagName('body')[0].style.backgroundColor="rgb(56, 162, 204)";    
+    }
+    else
+        document.getElementsByTagName('body')[0].style.backgroundColor="cyan";
 }
